3048334bea71d0c2333f0a588dd7bff55e4e3751
MilhamAt <M477ashiddiq@gmail.com>
2020-12-19 00:05:11 +0700

    add on off logic

---
+++ AutoLayoutUIKitOnly/Controller/MainController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  AutoLayoutUIKitOnly
+//
+//  Created by Muhammad Ilham Ashiddiq Tresnawan on 18/12/20.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    var mainView: MainView!
+    
+    var swct: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.mainView = MainView(frame: self.view.frame)
+        self.view = self.mainView
+        
+        self.title = "Main View"
+        
+        self.mainView.mainButton.addTarget(self, action: #selector(buttonGetPressed), for: .touchUpInside)
+    }
+
+    @objc
+    private func buttonGetPressed(){
+        self.mainView.textlabel.textColor = .green
+        swct += 1
+        if swct == 2 {
+            self.mainView.textlabel.textColor = .red
+            swct = 0
+        }
+    }
+}
+
---
+++ AutoLayoutUIKitOnly/View/MainView.swift
@@ -0,0 +1,60 @@
+//
+//  MainView.swift
+//  AutoLayoutUIKitOnly
+//
+//  Created by Muhammad Ilham Ashiddiq Tresnawan on 18/12/20.
+//
+
+import UIKit
+
+class MainView:UIView {
+    
+    let textlabel: UILabel = {
+        let label = UILabel()
+        label.text = "Test"
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let mainButton: UIButton = {
+        let button = UIButton(type: UIButton.ButtonType.custom)
+        button.setTitle("Press me", for: .normal)
+        button.backgroundColor = .gray
+        button.tintColor = .white
+        button.layer.cornerRadius = 8
+//        button.frame.size = CGSize(width: 120, height: 40)
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView(){
+        self.backgroundColor = .white
+        
+        self.addSubview(self.textlabel)
+        self.addSubview(self.mainButton)
+        
+        textlabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            textlabel.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor),
+            textlabel.centerYAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerYAnchor),
+        ])
+        
+        mainButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            mainButton.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor),
+            mainButton.topAnchor.constraint(equalTo: textlabel.bottomAnchor, constant: 80),
+            mainButton.heightAnchor.constraint(equalToConstant: 40),
+            mainButton.widthAnchor.constraint(equalToConstant: 120)
+            
+        ])
+    }
+}
